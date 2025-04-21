58fe0217ea243340c761c31198e188a3e01b4049
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2025-04-20 21:49:12 -0300

    Mini fix reinicio rotación al poolear

--- BurgerVRProject/Assets/Scripts/Pooling/SliceableSpawnableObject.cs
+++ BurgerVRProject/Assets/Scripts/Pooling/SliceableSpawnableObject.cs
@@ -14,10 +14,12 @@ public class SliceableSpawnableObject : SpawnableObject
         for (int i = 0; i < cutParts.Length; i++)
         {
             cutParts[i].transform.localPosition = new Vector3(0, (i * 0.015f), 0);
+            cutParts[i].transform.localRotation = Quaternion.Euler(0,0,0);
             cutParts[i].SetActive(false);
         }
         
         fullVersion.transform.localPosition = Vector3.zero;
+        fullVersion.transform.localRotation = Quaternion.Euler(0,0,0);
         fullVersion.SetActive(true);
     }
 }
