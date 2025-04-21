519ac33e473cb6711cda03ee81df493d940c1ac1
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2025-04-20 22:56:01 -0300

    Ketchup Bottle uses pool DONE
    Métodos de desaparición del proyectil WIP

--- BurgerVRProject/Assets/Scripts/KetchupBottle/KetchupBottle.cs
+++ BurgerVRProject/Assets/Scripts/KetchupBottle/KetchupBottle.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KetchupBottle : MonoBehaviour
 {
+    [SerializeField] private ObjectPool projectilePool;
+    [SerializeField] private SpawnableObject ketchupProjectile;
+
     [SerializeField] private float projectileSpeed;
-    [SerializeField] private GameObject ketchupProjectile;
     [SerializeField] private Transform bottlePoint;
+
+    private void Awake()
+    {
+        PoolableFactory.InitPool(projectilePool, ketchupProjectile, projectilePool.PoolMaxSize);
+    }
 
     public void Use()
     {
-        GameObject spawnedProjectile = Instantiate(ketchupProjectile, bottlePoint.position, bottlePoint.rotation);
-        spawnedProjectile.GetComponent<Rigidbody>().velocity = projectileSpeed * bottlePoint.forward;
-        Destroy(spawnedProjectile, 5f);
+        var spawnedProjectile = PoolableFactory.TryCreateObject(projectilePool, bottlePoint);
+        spawnedProjectile.GameObject.GetComponent<Rigidbody>().velocity = projectileSpeed * bottlePoint.forward;
     }
 }
