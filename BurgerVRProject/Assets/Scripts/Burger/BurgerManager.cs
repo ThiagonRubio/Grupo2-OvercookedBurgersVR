599c0339ceb729757eda04fa74a3f1806ceb82d6
Facundo Cancela <105611511+FacundoCancela@users.noreply.github.com>
2025-04-22 20:03:06 -0300

    la mejor hamburguesa del universo

--- BurgerVRProject/Assets/Scripts/Burger/BurgerManager.cs
+++ BurgerVRProject/Assets/Scripts/Burger/BurgerManager.cs
@@ -11,12 +11,14 @@ public class BurgerManager : MonoBehaviour
     private BurgerItem lastAttachedItem;
     public bool preparingBurger = false;
     public bool burgerReady = false;
+    public float pilaHeight;
     private Dictionary<IngredientType, bool> attachedIngredients = new Dictionary<IngredientType, bool>();
 
     void OnCollisionEnter(Collision collision)
     {
         BurgerItem item = collision.gameObject.GetComponent<BurgerItem>();
-        HandleCollision(item);
+        if (item != null)
+            HandleCollision(item);
     }
 
     public void HandleCollision(BurgerItem item)
@@ -26,60 +28,48 @@ public class BurgerManager : MonoBehaviour
         if (grab != null && grab.isSelected) return;
         if (attachedIngredients.ContainsKey(item.ingredientType)) return;
 
+        MeshRenderer mr = item.GetComponent<MeshRenderer>();
+        if (mr == null) mr = item.GetComponentInChildren<MeshRenderer>();
+        if (mr == null)
+            return;
+
+        float alto = mr.bounds.size.y;
+
+        float yOffset = item.ingredientType == IngredientType.PanSuperior
+        ? alto * 0.15f
+        : alto * 0.25f;
+
+
+        Vector3 pos = new Vector3(
+            initialAttachPoint.position.x,
+            pilaHeight + yOffset,
+            initialAttachPoint.position.z
+        );
+
+        item.Attach(transform, pos, initialAttachPoint.rotation);
+        pilaHeight += alto;
+        attachedIngredients[item.ingredientType] = true;
+        lastAttachedItem = item;
+
         if (!preparingBurger && item.ingredientType == IngredientType.PanInferior)
         {
-            Vector3 newPos = initialAttachPoint.position;
-            item.Attach(transform, newPos, initialAttachPoint.rotation);
-
             preparingBurger = true;
-            attachedIngredients[item.ingredientType] = true;
-            lastAttachedItem = item;
-
-            var managerGrab = GetComponent<XRGrabInteractable>();
-            if (managerGrab != null) managerGrab.enabled = false;
-            var transformer = GetComponent<XRGeneralGrabTransformer>();
-            if (transformer != null) transformer.enabled = false;
+            var g = GetComponent<XRGrabInteractable>();
+            if (g != null) g.enabled = false;
+            var t = GetComponent<XRGeneralGrabTransformer>();
+            if (t != null) t.enabled = false;
             var rb = GetComponent<Rigidbody>();
             if (rb != null) rb.isKinematic = true;
-
-            return;
         }
-
-        if (preparingBurger && !burgerReady)
+        else if (!burgerReady && item.ingredientType == IngredientType.PanSuperior)
         {
-            Collider prevCol = lastAttachedItem.GetComponent<Collider>();
-            Collider newCol = item.GetComponent<Collider>();
-
-            float prevExtY = prevCol.bounds.extents.y;
-            float newExtY = newCol.bounds.extents.y;
-            Vector3 basePos = lastAttachedItem.transform.position;
-
-            Vector3 attachPos = new Vector3(
-                basePos.x,
-                basePos.y + prevExtY + newExtY,
-                basePos.z
-            );
-
-            if (item.ingredientType == IngredientType.PanSuperior)
-            {
-                item.Attach(transform, attachPos, initialAttachPoint.rotation);
-                burgerReady = true;
-                attachedIngredients[item.ingredientType] = true;
-
-                var managerGrab2 = GetComponent<XRGrabInteractable>();
-                if (managerGrab2 != null) managerGrab2.enabled = true;
-                var transformer2 = GetComponent<XRGeneralGrabTransformer>();
-                if (transformer2 != null) transformer2.enabled = true;
-                var rb2 = GetComponent<Rigidbody>();
-                if (rb2 != null) rb2.isKinematic = false;
-            }
-            else
-            {
-                item.Attach(transform, attachPos, initialAttachPoint.rotation);
-                attachedIngredients[item.ingredientType] = true;
-            }
-
-            lastAttachedItem = item;
+            burgerReady = true;
+            var g = GetComponent<XRGrabInteractable>();
+            if (g != null) g.enabled = true;
+            var t = GetComponent<XRGeneralGrabTransformer>();
+            if (t != null) t.enabled = true;
+            var rb = GetComponent<Rigidbody>();
+            if (rb != null) rb.isKinematic = false;
         }
     }
 
@@ -88,6 +78,7 @@ public class BurgerManager : MonoBehaviour
         preparingBurger = false;
         burgerReady = false;
         lastAttachedItem = null;
+        pilaHeight = initialAttachPoint.position.y;
         attachedIngredients.Clear();
     }
 }
