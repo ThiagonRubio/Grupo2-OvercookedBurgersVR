afd8f080a4b77efcd15e37bf8dc3aebe2f27c812
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2025-04-30 23:40:59 -0300

    Toggle Snap turn Fix
    Ahora está hecho mucho más lindo y encima no se rompe más

--- BurgerVRProject/Assets/Scripts/Generic/ActivateSnapTurn.cs
+++ BurgerVRProject/Assets/Scripts/Generic/ActivateSnapTurn.cs
@@ -11,22 +11,64 @@ public class ActivateSnapTurn : MonoBehaviour
 
     [SerializeField] private ActionBasedSnapTurnProvider snapTurn;
     [SerializeField] private ActionBasedContinuousTurnProvider continuousTurn;
+
+    private bool _isSnapTurnActive;
     
     private void Start()
     {
         _toggleSnapTurn = inputActions.FindActionMap("XRI RightHand Interaction").FindAction("ActivateSnapTurn");
         _toggleSnapTurn.Enable();
-        _toggleSnapTurn.performed += ToggleSnapTurn;
+        _toggleSnapTurn.performed += ToggleTurnType;
+        
+        UseContinuousTurn();
     }
 
     private void OnDestroy()
     {
-        _toggleSnapTurn.performed -= ToggleSnapTurn;
+        _toggleSnapTurn.performed -= ToggleTurnType;
     }
 
-    private void ToggleSnapTurn(InputAction.CallbackContext context)
+    private void ToggleTurnType(InputAction.CallbackContext context)
     {
-        snapTurn.enabled = !snapTurn.enabled;
-        continuousTurn.enabled = !continuousTurn.enabled;
+        if (_isSnapTurnActive)
+        {
+            UseContinuousTurn();
+        }
+        else
+        {
+            UseSnapTurn();
+        }
+    }
+
+    private void UseSnapTurn()
+    {
+        if (continuousTurn != null)
+        {
+            continuousTurn.rightHandTurnAction.action?.Disable();
+            continuousTurn.enabled = false;
+        }
+
+        if (snapTurn != null)
+        {
+            snapTurn.enabled = true;
+            snapTurn.rightHandSnapTurnAction.action?.Enable();
+        }
+        _isSnapTurnActive = true;
+    }
+
+    private void UseContinuousTurn()
+    {
+        if (snapTurn != null)
+        {
+            snapTurn.rightHandSnapTurnAction.action?.Disable();
+            snapTurn.enabled = false;
+        }
+        
+        if (continuousTurn != null)
+        {
+            continuousTurn.enabled = true;
+            continuousTurn.rightHandTurnAction.action?.Enable();
+        }
+        _isSnapTurnActive = false;
     }
 }
