1051cfb0914a1b90384ca73dce35e54d2b3a5f5c
Facundo Cancela <105611511+FacundoCancela@users.noreply.github.com>
2025-05-20 21:09:27 -0300

    Order manager

--- BurgerVRProject/Assets/Scripts/Deliver/DeliveryManager.cs
+++ BurgerVRProject/Assets/Scripts/Deliver/DeliveryManager.cs
@@ -1,15 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : PoolReturnManager
 {
+    [SerializeField] private OrderManager orderManager;
+
     private void OnCollisionEnter(Collision collision)
     {
         var manager = collision.gameObject.GetComponent<BurgerManager>();
-        if (manager != null)
+        if (manager == null) return;
+        List<IngredientType> ingredients = new List<IngredientType>(manager.AttachedIngredients.Values);
+        if (orderManager.OrderExists(ingredients))
+        {
+            orderManager.RemoveOrder(ingredients);
             ReturnToPool(manager);
+        }
+        else
+        {
+            Debug.Log("Pedido no válido o inexistente");
+        }
     }
+
     protected override void ReturnToPool(BurgerManager manager)
     {
         base.ReturnToPool(manager);
