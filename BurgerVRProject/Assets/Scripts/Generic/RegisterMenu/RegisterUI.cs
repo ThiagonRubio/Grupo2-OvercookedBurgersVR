b3ce169a1684adc7e011578a25e7c01f3c2b2ea8
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2025-05-24 20:22:30 -0300

    Added Registe Menu to level

---
+++ BurgerVRProject/Assets/Scripts/Generic/RegisterMenu/RegisterUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterUI : MonoBehaviour
+{
+    private Animator _registerUIAnimator;
+    void Start()
+    {
+        _registerUIAnimator = GetComponent<Animator>();
+    }
+    
+    public void ToggleMenu()
+    {
+        _registerUIAnimator.SetTrigger("ButtonPressed");
+    }
+}
