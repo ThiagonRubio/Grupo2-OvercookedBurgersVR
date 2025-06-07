aeacc2d9ac34bb444734b72f136e1bec78506f64
SantinoBaldan <santino.bcs@gmail.com>
2025-06-07 17:48:34 -0300

    Custom Update Manager

---
+++ BurgerVRProject/Assets/Scripts/Custom Update/CustomUpdateManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomUpdateManager : MonoBehaviour
+{
+    public List<CustomMethods> methodsList;
+    public static CustomUpdateManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        for (int i = 0; i < methodsList.Count; i++)
+        {
+            methodsList[i].CustomAwake();
+        }
+
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < methodsList.Count; i++) 
+        {
+            methodsList[i].CustomStart();
+        }
+    }
+
+    private void Update()
+    {
+        for(int i = 0;i < methodsList.Count; i++)
+        {
+            methodsList[i].CustomUpdate();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for(int i = 0; i < methodsList.Count; i++)
+        {
+            methodsList[i].FixedUpdate();
+        }
+    }
+
+    public void AddToMethodList(CustomMethods customMethods)
+    {
+        if (!methodsList.Contains(customMethods))
+        {
+            methodsList.Add(customMethods);
+        }
+    }
+
+    public void RemoveFromMethodList(CustomMethods customMethods)
+    {
+        if (methodsList.Contains(customMethods))
+        {
+            methodsList.Remove(customMethods);
+        }
+    }
+}
