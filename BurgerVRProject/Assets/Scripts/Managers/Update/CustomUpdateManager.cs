8ec51aace518600be7038abdac4211afc88b1fab
Brian Czernikowski <90883778+BCzk@users.noreply.github.com>
2025-06-08 01:37:34 -0300

    Custom Update Manager - Impl.

---
+++ BurgerVRProject/Assets/Scripts/Managers/Update/CustomUpdateManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomUpdateManager : MonoBehaviour
+{
+    public static CustomUpdateManager Instance
+    {
+        get { return instance; }
+    }
+    private static CustomUpdateManager instance;
+
+    private readonly List<IUpdatable> updatables = new List<IUpdatable>();
+    private readonly List<IFixedUpdatable> fixedUpdatables = new List<IFixedUpdatable>();
+    private readonly List<ILateUpdatable> lateUpdatables = new List<ILateUpdatable>();
+
+    //---------------------------------//
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    private void Update()
+    {
+        foreach (IUpdatable updatable in updatables)
+        {
+            updatable.OnUpdate();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (IFixedUpdatable fixedUpdatable in fixedUpdatables)
+        {
+            fixedUpdatable.OnFixedUpdate();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach (ILateUpdatable lateUpdatable in lateUpdatables)
+        {
+            lateUpdatable.OnLateUpdate();
+        }
+    }
+
+
+    //---------------------------------//
+
+    public void Register(object obj)
+    {
+        if (obj is IUpdatable updatable)
+        {
+            updatables.Add(updatable);
+        }
+        if (obj is IFixedUpdatable fixedUpdatable)
+        {
+            fixedUpdatables.Add(fixedUpdatable);
+        }
+        if (obj is ILateUpdatable lateUpdatable)
+        {
+            lateUpdatables.Add(lateUpdatable);
+        }
+    }
+
+    public void Unregister(object obj)
+    {
+        if (obj is IUpdatable updatable)
+        {
+            updatables.Remove(updatable);
+        }
+        if (obj is IFixedUpdatable fixedUpdatable)
+        {
+            fixedUpdatables.Remove(fixedUpdatable);
+        }
+        if (obj is ILateUpdatable lateUpdatable)
+        {
+            lateUpdatables.Remove(lateUpdatable);
+        }
+    }
+}
