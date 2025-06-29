3105cda131c9b065526b3cfc4c141a0b31d64815
Brian Czernikowski <90883778+BCzk@users.noreply.github.com>
2025-06-29 15:59:23 -0300

    Implemented Sound on Cutting

--- BurgerVRProject/Assets/Scripts/Tools/Knife.cs
+++ BurgerVRProject/Assets/Scripts/Tools/Knife.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 public class Knife : MonoBehaviour
 {
     private ISliceable _auxSliceable;
+    private AudioSource cachedAudioSource;
+
+    private void Awake()
+    {
+        cachedAudioSource = GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         _auxSliceable = collision.gameObject.GetComponent<ISliceable>();
         if (_auxSliceable != null)
+        {
             _auxSliceable.Slice();
+            cachedAudioSource.Play();
+        }
+
     }
 }
